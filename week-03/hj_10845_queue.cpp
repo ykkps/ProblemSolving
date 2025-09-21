f12aa715ff0fb1b316dd6cee322e26836df4b48c
HJ <hyejin7729@gmail.com>
2025-09-21 17:44:26 +0900

    Adds hj's solution for BOJ 10845

---
+++ week-03/hj_10845_queue.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    queue<int> q;
+
+    while (n--)
+    {
+        string s;
+        cin >> s;
+
+        if (s == "push")
+        {
+            int x;
+            cin >> x;
+            q.push(x);
+        }
+        else if (s == "pop")
+        {
+            if (!q.empty())
+            {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+            else
+                cout << -1 << '\n';
+        }
+        else if (s == "size")
+            cout << q.size() << '\n';
+        else if (s == "empty")
+            cout << (q.empty() ? 1 : 0) << '\n';
+        else if (s == "front")
+            cout << (q.empty() ? -1 : q.front()) << '\n';
+        else if (s == "back")
+            cout << (q.empty() ? -1 : q.back()) << '\n';
+    }
+}
